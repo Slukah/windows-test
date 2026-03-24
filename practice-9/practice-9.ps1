b6495f6b866107e06114a75b459991ea27845aed
Slukah <114645602+Slukah@users.noreply.github.com>
2026-03-24 02:13:23 -0700

    Update practice-9.ps1
    fixed errors

--- practice-9/practice-9.ps1
+++ practice-9/practice-9.ps1
@@ -1,15 +1,8 @@
-$count = 0
+$counter = 0
+while (Get-Process -Name notepad -ErrorAction SilentlyContinue) {
+	Write-Host 'Notepad is running...'
+	Start-Sleep -Seconds 1
+	$counter++
+}
 
-do
-{
-    if (Get-Process -Name notepad -ErrorAction SilentlyContinue)
-    {
-        Write-Host "Notepad is running"
-        $count++
-        Start-Sleep -Seconds 1
-    }
-
-} while (Get-Process -Name notepad -ErrorAction SilentlyContinue)
-
-Write-Host "Notepad is no longer running"
-Write-Host "The statement was displayed $count times"
+Write-Host "$counter"
